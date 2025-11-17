f020c05bd5b037ac6ea269da7e0712e738283c65
michbur <michalburdukiewicz@gmail.com>
2025-11-17 23:04:21 +0100

    added more generalizations

--- run-test.R
+++ run-test.R
@@ -5,6 +5,7 @@ if(!require("callr")) {
 }
 
 source("test-Metaboanalyst.R")
+source("test-rmzTabM.R")
 
 all_files <- list.files("examples", full.names = TRUE, pattern = "mztab")
 
@@ -16,12 +17,11 @@ all_imports <- lapply(all_test_functions, function(ith_test_fun) {
   setNames(gsub("^test_", "", all_test_functions))
 
 import_df <- lapply(names(all_imports), function(ith_name) {
-  as.factor(all_imports[[ith_name]][[1]])
+  factor(all_imports[[ith_name]][[1]], levels = c("error", "something weird", "success"))
 }) |>
   do.call(cbind.data.frame, args = _) |>
   data.frame() |>
-  setNames(names(all_imports)) |>
-  cbind(file = basename(all_files), x = _)
+  setNames(names(all_imports)) 
 
 unlink("docs")
 rmarkdown::render("index.Rmd", output_dir = "docs")
---
+++ test-rmzTabM.R
@@ -0,0 +1,17 @@
+test_rmzTabM <- function(all_files) {
+  renv::activate(profile = "rmzTabM")
+  if(!require("rmzTabM")) {
+    renv::restore()
+    require("rmzTabM")
+  }
+  
+  all_reads <- lapply(all_files, function(ith_file_name) {
+    res <- try(readMzTab(ith_file_name), silent = TRUE)
+    res
+  })
+  
+  resulting_class <- sapply(all_reads, class)
+  
+  list(assessment = ifelse(resulting_class == "try-error", "error", "success"),
+                           reads = all_reads)
+}
