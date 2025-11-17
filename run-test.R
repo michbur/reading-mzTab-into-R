3b549622b1a3d08860b8bd35c757957a642781a6
michbur <michalburdukiewicz@gmail.com>
2025-11-17 22:36:20 +0100

    improved architecture

--- run-test.R
+++ run-test.R
@@ -1,6 +1,26 @@
-all_files <- list.files("examples", full.names = TRUE, pattern = "mztab")
+renv::activate(profile = "rendering")
 
 source("test-Metaboanalyst.R")
 
+all_files <- list.files("examples", full.names = TRUE, pattern = "mztab")
+
+all_test_functions <- ls()[grepl("^test_", ls())]
+
+all_imports <- lapply(all_test_functions, function(ith_test_fun) {
+  callr::r(get(ith_test_fun), args = list(all_files = all_files))
+}) |>
+  setNames(gsub("^test_", "", all_test_functions))
+
+import_df <- lapply(names(all_imports), function(ith_name) {
+  as.factor(all_imports[[ith_name]][[1]])
+}) |>
+  do.call(cbind.data.frame, args = _) |>
+  data.frame() |>
+  setNames(names(all_imports)) |>
+  cbind(file = basename(all_files), x = _)
+
 unlink("docs")
 rmarkdown::render("index.Rmd", output_dir = "docs")
+
+
+
--- test-Metaboanalyst.R
+++ test-Metaboanalyst.R
@@ -1,17 +1,28 @@
-renv::activate(profile = "MetaboAnalystR")
-if(!require("MetaboAnalystR")) {
-  renv::restore()
-  require("MetaboAnalystR")
+test_MetaboanalystR <- function(all_files) {
+  renv::activate(profile = "MetaboAnalystR")
+  if(!require("MetaboAnalystR")) {
+    renv::restore()
+    require("MetaboAnalystR")
+  }
+  
+  all_reads <- lapply(all_files, function(ith_file_name) {
+    # there are many global assignments in metaboanalystR 
+    # so we initiate and remove objects every loop and we clean the global
+    rm(list = setdiff(ls(), c("all_files", "ith_file_name")))
+    
+    # we use a non-standard anal.type to avoid Rserve initialization 
+    mSet <- InitDataObjects("mztab", "anything", FALSE, default.dpi = 150)
+    res <- try(Read.mzTab(mSet, ith_file_name, identifier = "name"), 
+               silent = TRUE)
+    res
+  })
+  
+  resulting_class <- sapply(all_reads, class)
+  
+  list(assessment = ifelse(resulting_class == "list", "success", 
+                           ifelse(resulting_class == "try-error", 
+                                  "error", "something weird")),
+       reads = all_reads)
 }
 
-all_reads <- lapply(all_files, function(ith_file_name) {
-  # there are many global assignments in metaboanalystR 
-  # so we initiate and remove objects every loop and we clean the global
-  rm(list = setdiff(ls(), c("all_files", "ith_file_name")))
-  
-  # we use a non-standard anal.type to avoid Rserve initialization 
-  mSet <- InitDataObjects("mztab", "anything", FALSE, default.dpi = 150)
-  res <- try(Read.mzTab(mSet, ith_file_name, identifier = "name"), 
-             silent = TRUE)
-  res
-})
+
